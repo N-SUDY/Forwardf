8de3853cbcb2a0cd9b08159a49e4f8ce6aa2686d
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-26 17:52:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
-FILE_CAPTION = environ.get('FILE_CAPTION', '<code>{file_name}</code>')
-OWNER = int(environ.get('OWNER', '5493832202'))
+API_ID = int(environ.get('API_ID', '11973721'))
+API_HASH = environ.get('API_HASH', '5264bf4663e9159565603522f58d3c18')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6078234170:AAGX4CDjIN2owsTeYTFOzk47XACzFO1nZSA')
+FILE_CAPTION = environ.get('FILE_CAPTION', '<b>{file_name}</b>')
+OWNER = int(environ.get('OWNER', '1391556668'))
 PRIVATE_BOT = bool(environ.get('PRIVATE_BOT', False))
+TARGET_DB = int(environ.get("TARGET_DB", '-1001849017994'))
