06d441e9279715768c9ace7d7f73c6469108f9ab
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-27 04:53:49 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,26 +5,18 @@ mongo = pymongo.MongoClient(DB_URL)
 db = mongo[DB_NAME]
 dbcol = db["user"]
 
-class Database:
-
 def set_caption(chat_id, caption):
        dbcol.update_one({"_id": chat_id},{"$set":{"caption": caption}})
 	
-def caption(chat_id): 
+def get_caption(chat_id): 
         dbcol.update_one({"_id": chat_id},{"$set":{"caption":None}})
     
-    async def (self, id, caption):
-        await self.col.update_one({'_id': int(id)}, {'$set': {'caption': caption}})
+def set_channel(server_id, target_chat_id):
+        dbcol.update_one({"serverid": server_id}, {"$set": {"target_chat_id": target_chat_id}})
 
-    async def get_caption(self, id):
-        user = await self.col.find_one({'_id': int(id)})
-        return user.get('caption', None)
-
-    async def set_channel(self, server_id, target_chat_id):
-        self.col.update_one({"serverid": server_id}, {"$set": {"target_chat_id": target_chat_id}})
-
-    async def get_channel(self, server_id):
-        user = await self.col.find_one({"serverid": server_id}, {"target_chat_id": 1})
+def get_channel(server_id):
+        user = await dbcol.find_one({"serverid": server_id}, {"target_chat_id": 1})
         return user.get("target_chat_id", None)
 
-db = Database(DB_URL, DB_NAME)
+def find_one(id):
+	return dbcol.find_one({"_id":id})
