99ca46cab3a209d2dc63a78f6c5cb35394300925
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-27 20:55:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,6 +3,7 @@ from os import environ
 API_ID = int(environ.get('API_ID', '11973721'))
 API_HASH = environ.get('API_HASH', '5264bf4663e9159565603522f58d3c18')
 BOT_TOKEN = environ.get('BOT_TOKEN', '6078234170:AAGX4CDjIN2owsTeYTFOzk47XACzFO1nZSA')
+OWNER = int(environ.get('OWNER', '1391556668'))
 FILE_CAPTION = environ.get('FILE_CAPTION', '<b>{file_name}</b>')
 TARGET_DB = int(environ.get("TARGET_DB", '-1001849017994'))
 DB_NAME = environ.get("DB_NAME","Cluster0")     
