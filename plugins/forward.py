c43c68c0f7479c0f811bdc146b371420ec3e42da
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-29 07:32:46 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -181,14 +181,14 @@ async def forward_files(lst_msg_id, chat, msg, bot, user_id):
                 continue
             try:
                 await bot.send_cached_media(
-                    chat_id=int(TARGET_DB),
+                    chat_id=CHANNEL.get(user_id) if CHANNEL.get(user_id) else TARGET_DB,
                     file_id=media.file_id,
                     caption=CAPTION.get(user_id).format(file_name=media.file_name, file_size=get_size(media.file_size), caption=message.caption) if CAPTION.get(user_id) else FILE_CAPTION.format(file_name=media.file_name, file_size=get_size(media.file_size), caption=message.caption)
                 )
             except FloodWait as e:
                 await asyncio.sleep(e.value)  # Wait "value" seconds before continuing
                 await bot.send_cached_media(
-                    chat_id=int(TARGET_DB),
+                    chat_id=CHANNEL.get(user_id) if CHANNEL.get(user_id) else TARGET_DB,
                     file_id=media.file_id,
                     caption=CAPTION.get(user_id).format(file_name=media.file_name, file_size=get_size(media.file_size), caption=message.caption) if CAPTION.get(user_id) else FILE_CAPTION.format(file_name=media.file_name, file_size=get_size(media.file_size), caption=message.caption)
                 )
